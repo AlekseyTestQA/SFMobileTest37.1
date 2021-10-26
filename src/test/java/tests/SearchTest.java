49f5713e0f78492e66418647fdab7d43f41efce3
Aleksey <dix-aleksey@yandex.ru>
2021-10-26 21:25:30 +0300

    Выполнение Пункта 2

---
+++ src/test/java/tests/SearchTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import lib.CoreTestCase;
+import lib.ui.MainPageObject;
+import lib.ui.pages.factory.OnboardingPageFactory;
+import lib.ui.pages.factory.SearchPageFactory;
+import lib.ui.pages.factory.StartPageFactory;
+import lib.ui.pages.interfaces.IOnboardingPageObject;
+import lib.ui.pages.interfaces.ISearchPageObject;
+import lib.ui.pages.interfaces.IStartPageObject;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import java.nio.file.WatchEvent;
+
+public class SearchTest extends CoreTestCase {
+    //Выполнение пункта 2
+    @Test
+    public void testSearch() {
+        IOnboardingPageObject onboardingPage = OnboardingPageFactory.get(this.driver);
+        IStartPageObject startPage = StartPageFactory.get(this.driver);
+        ISearchPageObject searchPage = SearchPageFactory.get(this.driver);
+
+        onboardingPage.skipOnboarding();
+        startPage.initSearch();
+        searchPage.findByText("Java");
+        searchPage.selectByText("Island of Indonesia");
+    }
+}
